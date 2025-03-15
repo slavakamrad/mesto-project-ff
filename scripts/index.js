b826372f4fa08bdd99663e57648c846645b7738b
Vyacheslav Lobanov <v-s-lobanov@yandex.ru>
2025-03-15 17:01:21 +0300

    add alt for card images

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,7 @@ function addCards(item, deleteCard) {
   const card = cardTemplate.querySelector(".places__item").cloneNode(true);
   card.querySelector(".card__title").textContent = item.name;
   card.querySelector(".card__image").src = item.link;
+  card.querySelector(".card__image").alt = item.name;
   card
     .querySelector(".card__delete-button")
     .addEventListener("click", function () {

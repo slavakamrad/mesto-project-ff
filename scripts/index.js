2df57cafb82f17f93068f1e951322863c008cf0d
Vyacheslav Lobanov <v-s-lobanov@yandex.ru>
2025-03-15 19:31:41 +0300

    add variable cardImage

--- scripts/index.js
+++ scripts/index.js
@@ -8,8 +8,9 @@ function deleteCard(card) {
 function addCards(item, deleteCard) {
   const card = cardTemplate.querySelector(".places__item").cloneNode(true);
   card.querySelector(".card__title").textContent = item.name;
-  card.querySelector(".card__image").src = item.link;
-  card.querySelector(".card__image").alt = item.name;
+  const cardImage = card.querySelector(".card__image")
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
   card
     .querySelector(".card__delete-button")
     .addEventListener("click", function () {
